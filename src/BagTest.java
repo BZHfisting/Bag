f2d216c24cd4066a61519f2cbfe38b1cc09bf9b3
AllanC56 <18035735+AllanC56@users.noreply.github.com>
2017-10-18 08:59:46 +0200

    Add BagTest class

---
+++ src/BagTest.java
@@ -0,0 +1,34 @@
+
+public class BagTest {
+    Integer[][] indexOccurences;
+    
+    public static void main(String[] args) {
+        Arrays.fill(indexOccurences, 0);
+        Bag bag1 = new Bag();
+        for (int nbExp = 0; i < nbExp; i++) {
+            bag1.addTenElem();
+
+        }
+        for (int i = 0; i < indexOccurences.size; i++) {
+
+        }
+    }
+
+    private void method() throws  {
+
+    }
+
+
+    private void addTenElem() {
+        this.add(new Element(new Integer(1)));
+        this.add(new Element(new Integer(2)));
+        this.add(new Element(new Integer(3)));
+        this.add(new Element(new Integer(4)));
+        this.add(new Element(new Integer(5)));
+        this.add(new Element(new Integer(6)));
+        this.add(new Element(new Integer(7)));
+        this.add(new Element(new Integer(8)));
+        this.add(new Element(new Integer(9)));
+        this.add(new Element(new Integer(10)));
+    }
+}
